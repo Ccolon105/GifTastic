7dfe52d0798d707cce356635bab523c088600412
Lawgiver17 <ccolon105@yahoo.com>
2018-11-26 21:57:28 -0700

    added read me and finished functionality

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # GifTastic
+
+This is a simple Gif finder. 
+
+You can type in your favorite animal and add it to the list of buttons. Once you click a button, you will see 10 Gifs populate the page related to that animal. The gif rating will display as well. 
+
+This project utilizes the Giphy API to get the gifs. 
+
+This project is maintained by Caleb Colon.
+
+Please reach out at ccolon105@yahoo.com with any questions. 
--- assets/javascript.js
+++ assets/javascript.js
@@ -6,19 +6,68 @@ function renderButtons(){
     for (var i =0; i < gifs.length; i++) {
         var a = $("<button>");
         a.addClass("gif-button");
-        a.attr("data-gif", gifs[i]);
+        a.addClass("btn btn-primary");
+        a.attr("data-animal", gifs[i]);
         a.text(gifs[i]);
         $("#button-view").append(a);
 
     }
 }
-$("#add-gif").on("click", function(event) {
-    event.preventDefault();
-  
-    var gif = $("#gif-input").val().trim();
 
-    gifs.push(gif);
-
+$("#add-animal").on("click", function(){
+    var animal = $("#gif-input").val().trim();
+    gifs.push(animal);
     renderButtons();
-  });
+    return false;
+})
+
+
+  function renderGifs() {
+    var animal = $(this).attr("data-animal");
+
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+      animal + "&api_key=jE6KMRWjffnZt4iNmU4NXWTcgqfSW2WX&limit=10";
+
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    })
+      .then(function(response) {
+
+        var results = response.data;
+      
+        for (var i = 0; i < results.length; i++) {
+
+          var animalDiv = $("<div>");
+
+          var p = $("<p>").text("Rating: " + results[i].rating);
+
+          var animalImage = $("<img>");
+
+          animalImage.attr("src", results[i].images.fixed_height_still.url);
+          animalImage.attr("data-still", results[i].images.fixed_height_still.url);
+          animalImage.attr("data-state", "still");
+          animalImage.addClass("gif");
+          animalImage.attr("data-animate", results[i].images.fixed_height.url);
+          
+          animalDiv.append(p);
+          animalDiv.append(animalImage);
+
+          $("#gif-view").prepend(animalDiv);
+        }
+    });
+    
+};
+$(document).on("click", ".gif", function(){
+    var state = $(this).attr("data-state");
+      if(state == "still"){
+        $(this).attr("src", $(this).data("animate"));
+        $(this).attr("data-state", "animate");
+      }else {
+        $(this).attr("src", $(this).data("still"));
+        $(this).attr("data-state", "still");
+      };
+});
+
+$(document).on("click", ".gif-button", renderGifs);
 renderButtons();
